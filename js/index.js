0cc91545105dc8fd13f7a1aee993b8186dde95bb
Gabs Ghidini <43341520+gabsghidini@users.noreply.github.com>
2022-05-18 11:55:16 -0300

    started working on DOM branch

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// RECUPERANDO INFORMAÇÕES BASE
+const itemsList = document.querySelector(".itemsList");
+
+
+/*
+
+<li class="card">
+  <figure class="img">
+    <img src="./img/jaqueta.svg" alt="Lightweight Jacket" title="Lightweight Jacket" class="">
+  </figure>
+  <div class="details">
+    <small class="itemCategory">Camisetas</small>
+    <h3 class="itemTitle">Lightweight Jacket</h3>
+    <p class="itemDescription">Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante...</p>
+    <h4 class="itemPrice">R$ 100.00</h4>
+    <button class="addCart">Adicionar ao carrinho</button>
+  </div>
+</li>
+
+*/
